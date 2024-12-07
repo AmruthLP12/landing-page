f9db991e5ce0924806a45c5544dad423000b0247
Amruth L P <amruthlp12@gmail.com>
2024-12-07 16:01:03 +0530

    NavBar completed

---
+++ constant/Constant.ts
@@ -0,0 +1,32 @@
+export const navLinks = [
+    {
+      id:1,
+      url: '/',
+      label: 'Home',
+    },
+    {
+      id:2,
+      url: '/about',
+      label: 'About',
+    },
+    {
+      id:3,
+      url: '/feature',
+      label: 'Feature',
+    },
+    {
+      id:4,
+      url: '/testimonial',
+      label: 'Testimonial',
+    },
+    {
+      id:5,
+      url: '/blog',
+      label: 'Blog',
+    },
+    {
+      id:1,
+      url: '/contact',
+      label: 'Contact',
+    },
+  ]
