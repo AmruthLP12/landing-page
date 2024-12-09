304d5b9dedd866b785b25307c1535b9634469b42
Amruth L P <amruthlp12@gmail.com>
2024-12-09 15:34:39 +0530

    Feature and review complete

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import Hero from './Hero/Hero'
-import WhyChoose from './WhyChoose/WhyChoose'
-import AnalyticsFeature from './AnalyticsFeature/AnalyticsFeature'
+import React from "react";
+import Hero from "./Hero/Hero";
+import WhyChoose from "./WhyChoose/WhyChoose";
+import AnalyticsFeature from "./AnalyticsFeature/AnalyticsFeature";
+import Feature from "./Feature/Feature";
+import Review from "./Review/Review";
 
 const Home = () => {
   return (
-    <div className='overflow-hidden'>
-      <Hero/>
-      <WhyChoose/>
-      <AnalyticsFeature/>
+    <div className="overflow-hidden">
+      <Hero />
+      <WhyChoose />
+      <AnalyticsFeature />
+      <Feature />
+      <Review />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
